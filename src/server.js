1c129a99a9d4f9bbdb8b9f6703d1d6bc4e9ed33d
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-01-26 19:26:26 +0530

    feat: routing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { router } = require('./routes');
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+app.use(router);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
